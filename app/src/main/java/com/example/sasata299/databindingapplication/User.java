b3a879044ea037158103ddd7661d20110bae2342
sasata299 <karaage299@gmail.com>
2016-12-04 11:01:23 +0900

    first commit

---
+++ app/src/main/java/com/example/sasata299/databindingapplication/User.java
@@ -0,0 +1,15 @@
+package com.example.sasata299.databindingapplication;
+
+/**
+ * Created by sasata299 on 2016/12/04.
+ */
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
