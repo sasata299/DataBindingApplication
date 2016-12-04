53701d9aab11fbad161b2b40f539247bdafd48bc
sasata299 <karaage299@gmail.com>
2016-12-04 12:17:14 +0900

    データを変更したときに動的にViewの表示も更新されるようにする
    http://qiita.com/hkurokawa/items/1c53c7de1c277b7f7675#observable-%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88

--- app/src/main/java/com/example/sasata299/databindingapplication/MainActivity.java
+++ app/src/main/java/com/example/sasata299/databindingapplication/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.sasata299.databindingapplication;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sasata299.databindingapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("Test", "User");
+
+        user = new User();
+        user.setFirstName("Test");
+        user.setLastName("User");
+
         binding.setUser(user);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                user.setFirstName("aaa");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sasata299/databindingapplication/User.java
+++ app/src/main/java/com/example/sasata299/databindingapplication/User.java
@@ -1,15 +1,31 @@
 package com.example.sasata299.databindingapplication;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by sasata299 on 2016/12/04.
  */
 
-public class User {
-    public final String firstName;
-    public final String lastName;
+public class User extends BaseObservable {
+    private String firstName;
+    private String lastName;
 
-    public User(String firstName, String lastName) {
+    @Bindable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
